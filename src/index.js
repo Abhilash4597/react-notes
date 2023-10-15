2d4d03b2d9c67587dbcdea3fbbb6f457bdabab2e
Abhilash Singh <abhilash4597@gmail.com>
2023-10-16 00:24:40 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function App(){
+  return <h1>Hello World...!!</h1>
+}
+
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 
-reportWebVitals();
