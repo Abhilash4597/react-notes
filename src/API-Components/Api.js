ba9660a848c681a22df4978e0b8fb791a791ff25
Abhilash Singh <abhilash4597@gmail.com>
2023-10-28 02:06:26 +0530

    API project update

---
+++ src/API-Components/Api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImages = async () => {
+    const response = await axios.get('https://api.unsplash.com/search/photos',{
+        headers:{
+            Authorization:'Client-ID I8NRLHkfLvwUxvgv20lhd_g2Rotzo90CJ6y0z2ldZT0'
+        },
+        params:{
+            query:'cars'
+        }
+    })
+    console.log(response)
+    return response;
+}
+
+export default searchImages;
