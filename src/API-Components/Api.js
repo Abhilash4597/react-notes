aa2b89cc06557600dc14c8abf0b652835ad93554
Abhilash Singh <abhilash4597@gmail.com>
2023-10-30 23:05:48 +0530

    update

--- src/API-Components/Api.js
+++ src/API-Components/Api.js
@@ -6,7 +6,7 @@ const searchImages = async () => {
             Authorization:'Client-ID I8NRLHkfLvwUxvgv20lhd_g2Rotzo90CJ6y0z2ldZT0'
         },
         params:{
-            query:'flowers'
+            query:'cars'
         }
     })
     console.log(response)
