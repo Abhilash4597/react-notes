58b8ac6f5b55e45c8d38e09fa4bb07d54ea020d5
Abhilash Singh <abhilash4597@gmail.com>
2023-10-31 03:04:47 +0530

    Api project complete

--- src/API-Components/ImageShow.js
+++ src/API-Components/ImageShow.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function ImageShow({image}) {
   return (
     <div>
-      <p>{image.alt_description}</p>
       <img src={image.urls.regular} alt={image.alt_description} />
     </div>
   )
