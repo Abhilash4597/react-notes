30c89afd40b9cc774feb352feca2302328c33a1d
Abhilash Singh <abhilash4597@gmail.com>
2023-10-31 02:55:20 +0530

    update

--- src/API-Components/ImageList.js
+++ src/API-Components/ImageList.js
@@ -4,7 +4,7 @@ import ImageShow from './ImageShow'
 export default function ImageList({images}) {
 
   const renderedImage = images.map((img)=>{
-    return <ImageShow image={img} />
+    return <ImageShow image={img} key={img.id}/>
   })
   return (
     <div>
--- src/API-Components/ImageShow.js
+++ src/API-Components/ImageShow.js
@@ -3,9 +3,8 @@ import React from 'react'
 export default function ImageShow({image}) {
   return (
     <div>
-      {console.log(image)}
       <p>{image.alt_description}</p>
-      <img src={image.urls.full} alt={image.alt_description} />
+      <img src={image.urls.regular} alt={image.alt_description} />
     </div>
   )
 }
