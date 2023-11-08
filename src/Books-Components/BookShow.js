d5249637625371d5848dc4cf5e5affc4de0108a7
Abhilash Singh <abhilash4597@gmail.com>
2023-11-08 22:10:13 +0530

    update

--- src/Books-Components/BookShow.js
+++ src/Books-Components/BookShow.js
@@ -28,7 +28,7 @@ export default function BookShow({book,onDelete,onEdit}) {
       <img src={`https://picsum.photos/seed/${book.id}300/200`} alt='Books' />
       <div>{content}</div>
       <div className='actions'>
-        <button className='edit' onClick={handleEditClick}>Delete</button>
+        <button className='edit' onClick={handleEditClick}>Edit</button>
         <button className='delete' onClick={handleDeleteClick}>Delete</button>
       </div>
     </div>
