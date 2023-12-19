0c803ffd59dd33d2df09a1db9a0c0fc7f8bdde4e
Abhilash Singh <abhilash4597@gmail.com>
2023-12-19 21:19:09 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import BooksContext from './Context/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App />
+    <BooksContext.Provider value={5}>
+        <App />
+    </BooksContext.Provider>
 );
 
 
