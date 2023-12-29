18b0343052316f45371ed6c832677a3a01be9fa1
Abhilash Singh <abhilash4597@gmail.com>
2023-12-29 23:07:58 +0530

    update

--- src/Context/books.js
+++ src/Context/books.js
@@ -2,8 +2,6 @@ import { createContext, useState } from "react";
 
 const BooksContext = createContext();
 
-// Just Practicing CreateContext
-
 function Provider({children}) {
     const [count, setCount] = useState(0);
 
@@ -17,7 +15,7 @@ function Provider({children}) {
     return <BooksContext.Provider value={valueToShare}>
         {children}
     </BooksContext.Provider>
-// Just Practicing CreateContext
+    
 }
 
 export {Provider};
